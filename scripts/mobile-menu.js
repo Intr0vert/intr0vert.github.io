67bc01acdea0fd7f37b59ebd64da4e7fa17d1828
Sergey Namestnikov <snamestnikov@alfabank.ru>
2022-09-27 17:04:57 +0300

    Added images and mobile modal animation

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -3,22 +3,21 @@ const MENU_ACTIVE = 'menu-active';
 const menu = document.getElementById('menu-mobile');
 
 const burgerButton = document.querySelector('.burger-icon');
-const closeButton = document.querySelector('.burger-close-icon');
 
 burgerButton.addEventListener('click', () => {
   document.body.classList.add('scrollOff')
 
-  burgerButton.classList.add(MENU_ACTIVE)
-  closeButton.classList.add(MENU_ACTIVE)
+  const isActive = burgerButton.classList.contains(MENU_ACTIVE)
+
+  if (isActive) {
+    burgerButton.classList.remove(MENU_ACTIVE);
+
+    menu.classList.remove(MENU_ACTIVE);
+
+    return;
+  }
+
+  burgerButton.classList.add(MENU_ACTIVE);
 
   menu.classList.add(MENU_ACTIVE);
-})
-
-closeButton.addEventListener('click', () => {
-  document.body.classList.remove('scrollOff')
-
-  burgerButton.classList.remove(MENU_ACTIVE)
-  closeButton.classList.remove(MENU_ACTIVE)
-
-  menu.classList.remove(MENU_ACTIVE)
 })
