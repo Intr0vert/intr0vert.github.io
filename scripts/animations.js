562307476fe5b6c8fa93e187713282da82d978ba
Sergey Namestnikov <snamestnikov@alfabank.ru>
2022-10-02 13:11:11 +0300

    clear

--- scripts/animations.js
+++
@@ -1,8 +0,0 @@
-const animation = bodymovin.loadAnimation({
-    container: document.getElementById('video-playContainer'),
-    path: 'assets/animations/play.json',
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    name: "Play animation",
-});
